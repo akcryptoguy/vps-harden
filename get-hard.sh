8c92aa29b00ff78f4860d965e4921dc84476e704
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2018-11-26 10:23:36 -0900

    Update get-hard.sh

--- get-hard.sh
+++ get-hard.sh
@@ -101,14 +101,13 @@ SSHDFILE='/etc/ssh/sshd_config'
 
 function update_upgrade() {
 # must manually enter OS Update banner because figlet not installed yet
-cat << "EOF" | tee -a "$LOGFILE"
 printf "${lightyellow}"
-printf "  ___  ____    _   _           _       _\n"
-printf " / _ \/ ___|  | | | |_ __   __| | __ _| |_ ___\n"
-printf "| | | \___ \  | | | | '_ \ / _` |/ _` | __/ _ \\n"
-printf "| |_| |___) | | |_| | |_) | (_| | (_| | ||  __/\n"
-printf "\___/|____/   \___/| .__/ \__,_|\__,_|\__\___|\n"
-printf "                    |_|\n"
+printf "  ___  ____    _   _           _       _\n" | tee -a "$LOGFILE"
+printf " / _ \/ ___|  | | | |_ __   __| | __ _| |_ ___\n" | tee -a "$LOGFILE"
+printf "| | | \___ \  | | | | '_ \ / _` |/ _` | __/ _ \\n" | tee -a "$LOGFILE"
+printf "| |_| |___) | | |_| | |_) | (_| | (_| | ||  __/\n" | tee -a "$LOGFILE"
+printf "\___/|____/   \___/| .__/ \__,_|\__,_|\__\___|\n" | tee -a "$LOGFILE"
+printf "                    |_|\n" | tee -a "$LOGFILE"
 echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
 echo " `date +%d.%m.%Y_%H:%M:%S` : INITIATING SYSTEM UPDATE " | tee -a "$LOGFILE"
 echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
