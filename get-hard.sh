7bb847317459b78b8c8fa7f56e9637ff7e823904
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-20 12:30:43 -0800

    Update get-hard.sh

--- get-hard.sh
+++ get-hard.sh
@@ -42,24 +42,24 @@ EOF
 # write to log only, no output on screen # echo  -e "---------------------------------------------------- \n" >> $LOGFILE 2>&1
 
 function setup_environment() {
-### add colors ###
-lightred='\033[1;31m'  # light red
-red='\033[0;31m'  # red
-lightgreen='\033[1;32m'  # light green
-green='\033[0;32m'  # green
-lightblue='\033[1;34m'  # light blue
-blue='\033[0;34m'  # blue
-lightpurple='\033[1;35m'  # light purple
-purple='\033[0;35m'  # purple
-lightcyan='\033[1;36m'  # light cyan
-cyan='\033[0;36m'  # cyan
-lightgray='\033[0;37m'  # light gray
-white='\033[1;37m'  # white
-brown='\033[0;33m'  # brown
-yellow='\033[1;33m'  # yellow
-darkgray='\033[1;30m'  # dark gray
-black='\033[0;30m'  # black
-nocolor='\033[0m'    # no color
+    ### define colors ###
+    lightred=$'\033[1;31m'  # light red
+    red=$'\033[0;31m'  # red
+    lightgreen=$'\033[1;32m'  # light green
+    green=$'\033[0;32m'  # green
+    lightblue=$'\033[1;34m'  # light blue
+    blue=$'\033[0;34m'  # blue
+    lightpurple=$'\033[1;35m'  # light purple
+    purple=$'\033[0;35m'  # purple
+    lightcyan=$'\033[1;36m'  # light cyan
+    cyan=$'\033[0;36m'  # cyan
+    lightgray=$'\033[0;37m'  # light gray
+    white=$'\033[1;37m'  # white
+    brown=$'\033[0;33m'  # brown
+    yellow=$'\033[1;33m'  # yellow
+    darkgray=$'\033[1;30m'  # dark gray
+    black=$'\033[0;30m'  # black
+    nocolor=$'\e[0m' # no color
 
 # Used this while testing color output
 # printf " ${lightred}Light Red${nocolor}\n"
