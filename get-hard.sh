290ec0b924b4240a112f812b2d284589f96b4380
AKcryptoGUY <45030509+akcryptoguy@users.noreply.github.com>
2019-10-20 16:47:13 -0800

    finish fixing ksplice-uptrack

--- get-hard.sh
+++ get-hard.sh
@@ -913,7 +913,8 @@ echo -e -n "${cyan}"
         echo -e -n "${lightgreen}"
         echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
         echo -e " $(date +%m.%d.%Y_%H:%M:%S) : KSPLICE UPTRACK INSTALLED" | tee -a "$LOGFILE"
-        echo -e "---------------------------------------------------- ${yellow}" | tee -a "$LOGFILE"
+        echo -e "---------------------------------------------------- " | tee -a "$LOGFILE"
+		echo -e -n "${yellow}"
         echo -e " ** Enabling autoinstall & correcting permissions ** " | tee -a "$LOGFILE"
         sed -i "s/autoinstall = no/autoinstall = yes/" /etc/uptrack/uptrack.conf
         chmod 755 /etc/cron.d/uptrack
@@ -953,14 +954,6 @@ else :
     echo -e "---------------------------------------------------- \n" | tee -a "$LOGFILE"
     echo -e -n "${nocolor}"
         fi
-
-# original steps I gathered
-# sudo apt-get install libgtk2-perl consolekit iproute libck-connector0 libcroco3 libglade2-0 libpam-ck-connector librsvg2-2 librsvg2-common python-cairo python-dbus python-gi python-glade2 python-gobject-2 python-gtk2 python-pycurl python-yaml dbus-x11 -y
-# sudo wget https://ksplice.oracle.com/uptrack/dist/xenial/ksplice-uptrack.deb
-# sudo dpkg -i ksplice-uptrack.deb
-# sudo sed -i "s/autoinstall = no/autoinstall = yes/" /etc/uptrack/uptrack.conf
-# sudo chmod 755 /etc/cron.d/uptrack
-# sudo uptrack-upgrade -y
 }
 
 ###################
