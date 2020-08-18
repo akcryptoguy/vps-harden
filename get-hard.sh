7c1963eeebbb051a7ee5e5f6118ef51074b6a49d
akcryptoguy <akcryptoguy+github@gmail.com>
2020-08-17 21:25:27 -0800

    Update get-hard.sh

--- get-hard.sh
+++ get-hard.sh
@@ -872,12 +872,13 @@ function google_auth() {
     echo -e -n "${lightcyan}"
     echo -e " You can increase the security of your VPS by installing a 2-factor"
     echo -e " authentication solution which will require a time-based, one-time"
-    echo -e " token in addition to your username and password.\n"
+    echo -e " token in addition to your username and password. This installation"
+    echo -e " requires you to use the Google Authenticator app on your phone.\n"
 
         echo -e -n "${cyan}"
             while :; do
             echo -e "\n"
-            read -n 1 -s -r -p " Would you like to perform these steps now? y/n  " GOOGLEAUTH
+            read -n 1 -s -r -p " Would you like to install Google 2FA Authentication? y/n  " GOOGLEAUTH
             if [[ ${GOOGLEAUTH,,} == "y" || ${GOOGLEAUTH,,} == "Y" || ${GOOGLEAUTH,,} == "N" || ${GOOGLEAUTH,,} == "n" ]]
             then
                 break
